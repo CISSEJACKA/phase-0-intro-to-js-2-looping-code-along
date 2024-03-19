c7eeb8e8bef25926ed50393d8dfd80ca91ad1045
JACKA <jackarcisse@gmail.com>
2024-03-19 19:35:13 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    let thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`; // Build the thank you message
+ thankYouMessages.push(message);   
+}
+return thankYouMessages;
+}
+let names = ["Ali", "Peter", "Chase"];
+let eventName = "birthday";
+let messages = writeCards(names, eventName);
+console.log(messages);
+
+    
+  
+
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  countDown(10);
